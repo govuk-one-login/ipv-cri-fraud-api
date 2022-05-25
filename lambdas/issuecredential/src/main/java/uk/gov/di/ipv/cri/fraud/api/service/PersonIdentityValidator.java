ce1ac831e8e90d27c992cb8d5ad93d34a48beceb
tal-nagra <tal.nagra@digital.cabinet-office.gov.uk>
2022-05-25 14:24:05 +0100

    Add basic validation implementation

--- lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/fraud/api/service/PersonIdentityValidator.java
+++ lambdas/issuecredential/src/main/java/uk/gov/di/ipv/cri/fraud/api/service/PersonIdentityValidator.java
@@ -1,12 +1,26 @@
 package uk.gov.di.ipv.cri.fraud.api.service;
 
+import com.nimbusds.oauth2.sdk.util.StringUtils;
 import uk.gov.di.ipv.cri.common.library.domain.personidentity.PersonIdentity;
 import uk.gov.di.ipv.cri.fraud.api.domain.ValidationResult;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 class PersonIdentityValidator {
     ValidationResult<List<String>> validate(PersonIdentity personIdentity) {
-        return ValidationResult.createValidResult();
+        List<String> validationErrors = new ArrayList<>();
+        if (StringUtils.isBlank(personIdentity.getFirstName())) {
+            validationErrors.add("firstname must not be null or empty");
+        }
+        if (StringUtils.isBlank(personIdentity.getSurname())) {
+            validationErrors.add("surname must not be null or empty");
+        }
+        if (Objects.isNull(personIdentity.getDateOfBirth())) {
+            validationErrors.add("date of birth must not be null");
+        }
+        // this implementation needs completing to validate all necessary fields
+        return new ValidationResult<>(validationErrors.isEmpty(), validationErrors);
     }
 }
