dcee4a646e293b674ebf832be9d87c321c190d80
Seamus McShane <seamus.mcshane@digital.cabinet-office.gov.uk>
2022-07-04 11:26:10 +0100

    KBV-628 Allow resolving and accessing the private API from the dev enviroment.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 ./gradlew
-sam validate -t infrastructure/lambda/template.yaml
+sam validate -t infrastructure/lambda/template.yaml --config-env dev
 sam build -t infrastructure/lambda/template.yaml --config-env dev
-sam deploy -t infrastructure/lambda/template.yaml --config-env dev --config-file samconfig.toml --no-fail-on-empty-changeset
+sam deploy --config-file samconfig.toml --no-fail-on-empty-changeset -t infrastructure/lambda/template.yaml --config-env dev
