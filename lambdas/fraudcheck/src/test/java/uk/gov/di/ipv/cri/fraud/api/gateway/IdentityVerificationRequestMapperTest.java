cbed1968e791b21d27099b38d2b98fb462f25611
Matt McCallion <matt.mccallion@digital.cabinet-office.gov.uk>
2022-08-19 16:45:34 +0100

    LIME-37 - added tests - minor refactors
    Signed-off-by: Matt McCallion <matt.mccallion@digital.cabinet-office.gov.uk>

--- lambdas/fraudcheck/src/test/java/uk/gov/di/ipv/cri/fraud/api/gateway/IdentityVerificationRequestMapperTest.java
+++ lambdas/fraudcheck/src/test/java/uk/gov/di/ipv/cri/fraud/api/gateway/IdentityVerificationRequestMapperTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 import uk.gov.di.ipv.cri.common.library.domain.personidentity.PersonIdentity;
 import uk.gov.di.ipv.cri.fraud.api.gateway.dto.request.Address;
 import uk.gov.di.ipv.cri.fraud.api.gateway.dto.request.IdentityVerificationRequest;
+import uk.gov.di.ipv.cri.fraud.api.gateway.dto.request.PEPRequest;
 import uk.gov.di.ipv.cri.fraud.api.gateway.dto.request.Person;
 import uk.gov.di.ipv.cri.fraud.api.util.TestDataCreator;
 
@@ -110,6 +111,123 @@ class IdentityVerificationRequestMapperTest {
         assertEquals(personIdentity.getSurname(), person.getNames().get(0).getSurName());
 
         assertEquals("WEB", result.getPayload().getSource());
+
+        assertNotEquals(0, addressCount);
+
+        List<Address> addresses = result.getPayload().getContacts().get(0).getAddresses();
+
+        IntStream.range(0, addressCount)
+                .forEach(
+                        a -> {
+                            assertEquals(
+                                    a == 0 ? CURRENT.toString() : PREVIOUS.toString(),
+                                    addresses.get(a).getAddressType());
+                            assertEquals("PostTown" + a, addresses.get(a).getPostTown());
+                            assertEquals("Street Name" + a, addresses.get(a).getStreet());
+                            assertEquals("Postcode" + a, addresses.get(a).getPostal());
+                        });
+    }
+
+    @Test
+    void shouldThrowExceptionWhenPepPersonIdentityIsNull() {
+        NullPointerException exception =
+                assertThrows(
+                        NullPointerException.class,
+                        () -> requestMapper.mapPersonIdentity(personIdentity));
+        assertEquals("The personIdentity must not be null", exception.getMessage());
+    }
+
+    @Test
+    void shouldConvertPepPersonIdentityToCrossCoreApiRequestForCurrentAddress() {
+        personIdentity = TestDataCreator.createTestPersonIdentity(CURRENT);
+        personIdentity.getAddresses().get(0).setSubBuildingName("Building One");
+        personIdentity.getAddresses().get(0).setBuildingName("House Name");
+        personIdentity.getAddresses().get(0).setBuildingNumber("44");
+
+        PEPRequest result = requestMapper.mapPEPPersonIdentity(personIdentity);
+
+        assertNotNull(result);
+
+        Person person = result.getPayload().getContacts().get(0).getPerson();
+
+        assertEquals(
+                LocalDate.of(1976, 12, 26).toString(), person.getPersonDetails().getDateOfBirth());
+        assertEquals(personIdentity.getFirstName(), person.getNames().get(0).getFirstName());
+        assertEquals(personIdentity.getSurname(), person.getNames().get(0).getSurName());
+        assertEquals("Y", person.getPersonDetails().getPepsSanctionsFlag());
+        assertNotNull(person.getPersonDetails().getYearOfBirth());
+        assertEquals("APPLICANT", person.getTypeOfPerson());
+
+        assertEquals(
+                "MS_CON",
+                result.getPayload().getApplication().getProductDetails().getProductCode());
+
+        Address address = result.getPayload().getContacts().get(0).getAddresses().get(0);
+
+        assertEquals(CURRENT.toString(), address.getAddressType());
+        assertEquals("PostTown", address.getPostTown());
+        assertEquals("Street Name", address.getStreet());
+        assertEquals("Postcode", address.getPostal());
+        assertEquals("Building One", address.getSubBuilding());
+        assertEquals("House Name", address.getBuildingName());
+        assertEquals("44", address.getBuildingNumber());
+    }
+
+    @Test
+    void shouldConvertPepPersonIdentityToCrossCoreApiRequestForPreviousAddress() {
+        personIdentity = TestDataCreator.createTestPersonIdentity(PREVIOUS);
+
+        PEPRequest result = requestMapper.mapPEPPersonIdentity(personIdentity);
+
+        assertNotNull(result);
+
+        Person person = result.getPayload().getContacts().get(0).getPerson();
+
+        assertEquals(
+                LocalDate.of(1976, 12, 26).toString(), person.getPersonDetails().getDateOfBirth());
+        assertEquals(personIdentity.getFirstName(), person.getNames().get(0).getFirstName());
+        assertEquals(personIdentity.getSurname(), person.getNames().get(0).getSurName());
+        assertEquals("Y", person.getPersonDetails().getPepsSanctionsFlag());
+        assertNotNull(person.getPersonDetails().getYearOfBirth());
+        assertEquals("APPLICANT", person.getTypeOfPerson());
+
+        assertEquals(
+                "MS_CON",
+                result.getPayload().getApplication().getProductDetails().getProductCode());
+
+        Address address = result.getPayload().getContacts().get(0).getAddresses().get(0);
+
+        assertEquals(PREVIOUS.toString(), address.getAddressType());
+        assertEquals("PostTown", address.getPostTown());
+        assertEquals("Street Name", address.getStreet());
+        assertEquals("Postcode", address.getPostal());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getAddressCount")
+    void shouldConvertPepPersonIdentityToCrossCoreApiRequestWithAddressCount(int addressCount) {
+
+        personIdentity =
+                TestDataCreator.createTestPersonIdentityMultipleAddresses(
+                        addressCount, 0, 0, false);
+
+        PEPRequest result = requestMapper.mapPEPPersonIdentity(personIdentity);
+
+        assertNotNull(result);
+
+        Person person = result.getPayload().getContacts().get(0).getPerson();
+
+        assertEquals(
+                LocalDate.of(1976, 12, 26).toString(), person.getPersonDetails().getDateOfBirth());
+        assertEquals(personIdentity.getFirstName(), person.getNames().get(0).getFirstName());
+        assertEquals(personIdentity.getSurname(), person.getNames().get(0).getSurName());
+        assertEquals("Y", person.getPersonDetails().getPepsSanctionsFlag());
+        assertNotNull(person.getPersonDetails().getYearOfBirth());
+        assertEquals("APPLICANT", person.getTypeOfPerson());
+
+        assertEquals(
+                "MS_CON",
+                result.getPayload().getApplication().getProductDetails().getProductCode());
 
         assertNotEquals(0, addressCount);
 
