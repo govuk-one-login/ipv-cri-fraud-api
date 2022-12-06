8df4106025d7e258b930e6f04f2f874f0420e240
Jamie Bentham <jamie.bentham@digital.cabinet-office.gov.uk>
2022-12-06 18:12:10 +0000

    LIME-117: Updated run-tests and build image to assume aws credentials

--- acceptance-tests/run-tests.sh
+++ acceptance-tests/run-tests.sh
@@ -1,13 +1,37 @@
 #!/usr/bin/env bash
 
-set -eu
-
 REPORT_DIR="${TEST_REPORT_DIR:=$PWD}"
 
 export BROWSER="${BROWSER:-chrome-headless}"
 export ENVIRONMENT="${ENVIRONMENT:-build}"
 export NO_CHROME_SANDBOX=true
-export STACK_NAME="${CFN_StackName:-local}"
+
+
+# Added to accommodate ssm stack
+if [[ -z "${CFN_StackName}" ]]; then
+  if [[ -z "${SAM_STACK_NAME}" ]]; then
+    export STACK_NAME="local"
+  else
+    export STACK_NAME="${SAM_STACK_NAME}"
+  fi
+else
+  export STACK_NAME="${CFN_StackName}"
+fi
+
+# Added to accommodate ssm stack
+if [[ -z "${ENVIRONMENT}" ]]; then
+  if [[ -z "${TEST_ENVIRONMENT}" ]]; then
+    export ENVIRONMENT="build"
+  else
+    export ENVIRONMENT="${TEST_ENVIRONMENT}"
+  fi
+else
+  export ENVIRONMENT="${ENVIRONMENT}"
+fi
+
+echo "ENVIRONMENT ${ENVIRONMENT}"
+echo "STACK_NAME ${STACK_NAME}"
+
 export JOURNEY_TAG=$(aws ssm get-parameter --name "/tests/${STACK_NAME}/TestTag" | jq -r ".Parameter.Value")
 
 PARAMETERS_NAMES=(coreStubPassword coreStubUrl coreStubUsername passportCriUrl apiBaseUrl orchestratorStubUrl)
