46dcc2999718eb5eed1189a765eda6c0bf0ab3f8
chinmayi-gds <chinmayi.shetty@digital.cabinet-office.gov.uk>
2022-12-13 16:18:47 +0000

    updated the getPrivateAPIEndpoint method

--- acceptance-tests/src/test/java/gov/di_ipv_fraud/pages/FraudAPIPage.java
+++ acceptance-tests/src/test/java/gov/di_ipv_fraud/pages/FraudAPIPage.java
@@ -29,19 +29,6 @@ public class FraudAPIPage {
             new ConfigurationService(System.getenv("ENVIRONMENT"));
     private static final Logger LOGGER = Logger.getLogger(FraudAPIStepDefs.class.getName());
 
-//    public void userIdentityAsJwtString(String criId)
-//            throws URISyntaxException, IOException, InterruptedException {
-//        String coreStubUrl = System.getenv("coreStubUrl");
-//        LOGGER.info("STUB URL = " + coreStubUrl);
-//        if (coreStubUrl == null) {
-//            throw new IllegalArgumentException("Environment variable IPV_CORE_STUB_URL is not set");
-//        }
-//
-//        String jsonString = getClaimsForUser(coreStubUrl, criId, LindaDuffExperianRowNumber);
-//        SESSION_REQUEST_BODY = createRequest(coreStubUrl, criId, jsonString);
-//        LOGGER.info("SESSION_REQUEST_BODY = " + SESSION_REQUEST_BODY);
-//    }
-
     public void userIdentityAsJwtString(String criId)
             throws URISyntaxException, IOException, InterruptedException {
         String coreStubUrl = configurationService.getCoreStubUrl(false);
@@ -54,24 +41,6 @@ public class FraudAPIPage {
         SESSION_REQUEST_BODY = createRequest(coreStubUrl, criId, jsonString);
         LOGGER.info("SESSION_REQUEST_BODY = " + SESSION_REQUEST_BODY);
     }
-
-//    public void postRequestToSessionEndpoint(String env)
-//            throws IOException, InterruptedException {
-//        LOGGER.info("getPrivateAPIEndpoint() ==> " + getPrivateAPIEndpoint(env));
-//        HttpRequest request =
-//                HttpRequest.newBuilder()
-//                        .uri(URI.create(getPrivateAPIEndpoint(env) + "/session"))
-//                        .setHeader("Accept", "application/json")
-//                        .setHeader("Content-Type", "application/json")
-//                        //                        .setHeader("X-Forwarded-For", "192.168.0.1")
-//                        .POST(HttpRequest.BodyPublishers.ofString(SESSION_REQUEST_BODY))
-//                        .build();
-//        String sessionResponse = sendHttpRequest(request).body();
-//        LOGGER.info("sessionResponse = " + sessionResponse);
-//        Map<String, String> deserialisedResponse =
-//                objectMapper.readValue(sessionResponse, new TypeReference<>() {});
-//        SESSION_ID = deserialisedResponse.get("session_id");
-//    }
 
     public void postRequestToSessionEndpoint()
             throws IOException, InterruptedException {
@@ -92,50 +61,10 @@ public class FraudAPIPage {
         SESSION_ID = deserialisedResponse.get("session_id");
     }
 
-//    public void getSessionId() {
-//        LOGGER.info("SESSION_ID = " + SESSION_ID);
-//        assertTrue(StringUtils.isNotBlank(SESSION_ID));
-//    }
-
     public void getSessionId() {
         LOGGER.info("SESSION_ID = " + SESSION_ID);
         assertTrue(StringUtils.isNotBlank(SESSION_ID));
     }
-
-//    private String getPrivateAPIEndpoint(String env) {
-//        String privateAPIEndpoint = System.getenv("apiGatewayIdPrivate");
-//        if (privateAPIEndpoint == null) {
-//            throw new IllegalArgumentException(
-//                    "Environment variable PRIVATE API endpoint is not set");
-//        }
-//        LOGGER.info("privateAPIEndpoint =>" + privateAPIEndpoint);
-//        return "https://" + privateAPIEndpoint + ".execute-api.eu-west-2.amazonaws.com/" + env;
-//    }
-
-//    private String getClaimsForUser(String baseUrl, String criId, int userDataRowNumber)
-//            throws URISyntaxException, IOException, InterruptedException {
-//        var url =
-//                new URI(
-//                        baseUrl
-//                                + "backend/generateInitialClaimsSet?cri="
-//                                + criId
-//                                + "&rowNumber="
-//                                + userDataRowNumber);
-//
-//        LOGGER.info("URL =>> " + url);
-//
-//        HttpRequest request =
-//                HttpRequest.newBuilder()
-//                        .uri(url)
-//                        .GET()
-//                        .setHeader(
-//                                "Authorization",
-//                                getBasicAuthenticationHeader(
-//                                        System.getenv("coreStubUsername"),
-//                                        System.getenv("coreStubPassword")))
-//                        .build();
-//        return sendHttpRequest(request).body();
-//    }
 
     private String getClaimsForUser(String baseUrl, String criId, int userDataRowNumber)
             throws URISyntaxException, IOException, InterruptedException {
@@ -163,27 +92,6 @@ public class FraudAPIPage {
         return sendHttpRequest(request).body();
     }
 
-//    private String createRequest(String baseUrl, String criId, String jsonString)
-//            throws URISyntaxException, IOException, InterruptedException {
-//
-//        URI uri = new URI(baseUrl + "backend/createSessionRequest?cri=" + criId);
-//        LOGGER.info("jsonString = " + jsonString);
-//        HttpRequest request =
-//                HttpRequest.newBuilder()
-//                        .uri(uri)
-//                        .setHeader("Accept", "application/json")
-//                        .setHeader("Content-Type", "application/json")
-//                        .setHeader(
-//                                "Authorization",
-//                                getBasicAuthenticationHeader(
-//                                        System.getenv("coreStubUsername"),
-//                                        System.getenv("coreStubPassword")))
-//                        .POST(HttpRequest.BodyPublishers.ofString(jsonString))
-//                        .build();
-//
-//        return sendHttpRequest(request).body();
-//    }
-
     private String createRequest(String baseUrl, String criId, String jsonString)
             throws URISyntaxException, IOException, InterruptedException {
 
@@ -205,24 +113,12 @@ public class FraudAPIPage {
         return sendHttpRequest(request).body();
     }
 
-//    private HttpResponse<String> sendHttpRequest(HttpRequest request)
-//            throws IOException, InterruptedException {
-//        HttpClient client = HttpClient.newBuilder().build();
-//        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-//        return response;
-//    }
-
     private HttpResponse<String> sendHttpRequest(HttpRequest request)
             throws IOException, InterruptedException {
         HttpClient client = HttpClient.newBuilder().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         return response;
     }
-
-//    private static final String getBasicAuthenticationHeader(String username, String password) {
-//        String valueToEncode = username + ":" + password;
-//        return "Basic " + Base64.getEncoder().encodeToString(valueToEncode.getBytes());
-//    }
 
     private static final String getBasicAuthenticationHeader(String username, String password) {
         String valueToEncode = username + ":" + password;
--- acceptance-tests/src/test/resources/features/FraudCRIAPI.feature
+++ acceptance-tests/src/test/resources/features/FraudCRIAPI.feature
@@ -5,10 +5,10 @@ Feature: Fraud CRI API
   Scenario: Acquire initial JWT (STUB)
     Given user has the user identity in the form of a signed JWT string for CRI Id fraud-cri-dev
     When user sends a POST request to session end point
-#    Then user gets a session-id
+    Then user gets a session-id
 
-#  @intialJWT_happy_path
-#  Scenario: Acquire initial JWT (STUB)
-#    Given user has the user identity in the form of a signed JWT string for CRI Id fraud-cri-build
-#    When user sends a POST request to session end point
-#    Then user gets a session-id
+  @intialJWT_happy_path
+  Scenario: Acquire initial JWT (STUB)
+    Given user has the user identity in the form of a signed JWT string for CRI Id fraud-cri-build
+    When user sends a POST request to session end point
+    Then user gets a session-id
