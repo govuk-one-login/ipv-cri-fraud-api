83383ecd88440c2b6adca724200501ae8516efb5
chinmayi-gds <chinmayi.shetty@digital.cabinet-office.gov.uk>
2023-02-21 10:41:54 +0000

    moved the Fraud API tests to a new branch

---
+++ acceptance-tests/src/test/java/gov/di_ipv_fraud/model/AuthorizationCode.java
@@ -0,0 +1,14 @@
+package gov.di_ipv_fraud.model;
+
+public class AuthorizationCode {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
