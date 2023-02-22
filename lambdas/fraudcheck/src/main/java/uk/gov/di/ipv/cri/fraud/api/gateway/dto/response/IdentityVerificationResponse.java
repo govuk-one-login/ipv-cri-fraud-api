9a09a8ab097275515135cca13c74beaa10dd5afb
ElliotMurphyGDS <elliot.murphy@digital.cabinet-office.gov.uk>
2023-02-22 11:51:25 +0000

    LIME-468 - Pinning gradle version to fix github actions

--- lambdas/fraudcheck/src/main/java/uk/gov/di/ipv/cri/fraud/api/gateway/dto/response/IdentityVerificationResponse.java
+++ lambdas/fraudcheck/src/main/java/uk/gov/di/ipv/cri/fraud/api/gateway/dto/response/IdentityVerificationResponse.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@JsonIgnoreProperties(value = "originalRequestData")
+@JsonIgnoreProperties(ignoreUnknown = true, value = "originalRequestData")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class IdentityVerificationResponse {
     @JsonProperty("responseHeader")
--- lambdas/fraudcheck/src/main/java/uk/gov/di/ipv/cri/fraud/api/gateway/dto/response/ResponseHeader.java
+++ lambdas/fraudcheck/src/main/java/uk/gov/di/ipv/cri/fraud/api/gateway/dto/response/ResponseHeader.java
@@ -1,9 +1,11 @@
 package uk.gov.di.ipv.cri.fraud.api.gateway.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ResponseHeader {
 
     @JsonProperty("requestType")
