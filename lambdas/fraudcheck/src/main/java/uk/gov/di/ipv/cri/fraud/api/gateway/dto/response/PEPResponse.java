cabaa7858c5bf09908b9868385aabd2de92df922
ElliotMurphyGDS <elliot.murphy@digital.cabinet-office.gov.uk>
2023-02-22 11:51:25 +0000

    LIME-468 - Pinning gradle version to fix github actions

--- lambdas/fraudcheck/src/main/java/uk/gov/di/ipv/cri/fraud/api/gateway/dto/response/PEPResponse.java
+++ lambdas/fraudcheck/src/main/java/uk/gov/di/ipv/cri/fraud/api/gateway/dto/response/PEPResponse.java
@@ -3,6 +3,6 @@ package uk.gov.di.ipv.cri.fraud.api.gateway.dto.response;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonIgnoreProperties(value = "originalRequestData")
+@JsonIgnoreProperties(ignoreUnknown = true, value = "originalRequestData")
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PEPResponse extends IdentityVerificationResponse {}
