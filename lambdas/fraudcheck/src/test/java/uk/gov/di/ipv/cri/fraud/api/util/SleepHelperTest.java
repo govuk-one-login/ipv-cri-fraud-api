7cea369484ba753f434ab7d2e509fa9c823cc9fe
Seamus McShane <seamus.mcshane@digital.cabinet-office.gov.uk>
2023-03-27 15:50:13 +0100

    LIME-238 Investigate api gateway latency
    	- This replaces use of Thread.sleep() with a busy wait.
    	- Corrects the first attempt never being ~0ms, due to using 2^tryCount.
    	- Added unit tests for sleep helper.

---
+++ lambdas/fraudcheck/src/test/java/uk/gov/di/ipv/cri/fraud/api/util/SleepHelperTest.java
@@ -0,0 +1,92 @@
+package uk.gov.di.ipv.cri.fraud.api.util;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class SleepHelperTest {
+
+    private SleepHelper sleepHelper;
+
+    private final long MAX_TEST_SLEEP = 6400L;
+
+    // A margin to account for processing speed/delays
+    // Waits can be intermittently slower depending
+    // on what is happening when tests are running
+    private final long EPSILON = 20L;
+
+    @BeforeEach
+    void setUp() {
+        sleepHelper = new SleepHelper(MAX_TEST_SLEEP);
+    }
+
+    @Test
+    void shouldBusyWait0msWhenCalledOnce() {
+        long expectedWait = 0;
+
+        long waitTime = sleepHelper.busyWaitWithExponentialBackOff(0);
+
+        assertEquals(expectedWait, waitTime, EPSILON);
+    }
+
+    @Test
+    void shouldSleep0msWhenCalledOnce() throws InterruptedException {
+        long expectedWait = 0;
+
+        long waitTime = sleepHelper.sleepWithExponentialBackOff(0);
+
+        assertEquals(expectedWait, waitTime, EPSILON);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7})
+    void shouldBusyWait2P100ForCallN(int callNumber) {
+        long baseWaitTime = 100;
+        long power = callNumber - 1;
+
+        long expectedWait = (long) Math.pow(2, power) * baseWaitTime;
+
+        long waitTime = sleepHelper.busyWaitWithExponentialBackOff(callNumber);
+
+        assertEquals(expectedWait, waitTime, EPSILON);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3, 4, 5, 6, 7})
+    void shouldSleep2P100ForCallN(int callNumber) throws InterruptedException {
+        long baseWaitTime = 100;
+        long power = callNumber - 1;
+
+        long expectedWait = (long) Math.pow(2, power) * baseWaitTime;
+
+        long waitTime = sleepHelper.sleepWithExponentialBackOff(callNumber);
+
+        assertEquals(expectedWait, waitTime, EPSILON);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {8, 9})
+    void shouldBusyWaitMaxTimeWhenCalledMoreThan7Times(int callNumber) {
+        long expectedWait = MAX_TEST_SLEEP;
+
+        long waitTime = sleepHelper.busyWaitWithExponentialBackOff(callNumber);
+
+        assertEquals(expectedWait, waitTime, EPSILON);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {8, 9})
+    void shouldSleepMaxTimeWhenCalledMoreThan7Times(int callNumber) throws InterruptedException {
+        long expectedWait = MAX_TEST_SLEEP;
+
+        long waitTime = sleepHelper.sleepWithExponentialBackOff(callNumber);
+
+        assertEquals(expectedWait, waitTime, EPSILON);
+    }
+}
