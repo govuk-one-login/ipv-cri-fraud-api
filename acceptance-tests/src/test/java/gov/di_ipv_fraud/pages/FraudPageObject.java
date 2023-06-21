225186b656108457f535b5e130f99d851f3092b9
MathumithaVe <mathumitha.venkatachala@digital.cabinet-office.gov.uk>
2023-06-21 12:42:47 +0100

    Spotless Apply

--- acceptance-tests/src/test/java/gov/di_ipv_fraud/pages/FraudPageObject.java
+++ acceptance-tests/src/test/java/gov/di_ipv_fraud/pages/FraudPageObject.java
@@ -27,7 +27,6 @@ import static gov.di_ipv_fraud.utilities.BrowserUtils.checkOkHttpResponseOnLink;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FraudPageObject extends UniversalSteps {
